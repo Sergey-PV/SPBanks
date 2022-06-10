21206c836be98515597e5f6b5d42d241c71a1560
Sergey Parfenchyk <sergey.parfenchyk@gmail.com>
2022-06-10 21:23:35 +0300

    Initial project setup

---
+++ SPBanks/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  SPBanks
+//
+//  Created by Sergey Parfentchik on 10.06.22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow.init(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+    }
+
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
---
+++ SPBanks/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SPBanks
+//
+//  Created by Sergey Parfentchik on 10.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
